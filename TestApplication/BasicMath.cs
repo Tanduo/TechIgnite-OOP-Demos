cf171216847415a346ab2627da3bacb2c8cf0378
Ryan Lange <56279086+rpl17@users.noreply.github.com>
2023-10-07 06:04:39 -0500

    Initial Commit

---
+++ TestApplication/BasicMath.cs
@@ -0,0 +1,34 @@
+﻿namespace TestApplication
+{
+	public class BasicMath
+	{
+		public BasicMath()
+		{
+		}
+        //Returns the sum of inputs
+        public int Add(int input, int input2)
+        {
+            return input + input2;
+        }
+
+        //Returns the product of inputs
+        public int Multiply(int input, int input2)
+        {
+            return input * input2;
+        }
+
+        //Returns the input squared
+        public int Square(int input)
+        {
+            return input * input;
+        }
+
+        //Returns the square root of the input
+        public int SquareRoot(int input)
+        {
+            return (int)Math.Sqrt(input);
+        }
+
+    }
+}
+
---
+++ TestApplication/Calculator.cs
@@ -0,0 +1,23 @@
+﻿namespace TestApplication
+{
+	public class Calculator : BasicMath
+	{
+		public Calculator()
+		{
+		}
+
+		public void powerOn()
+		{
+			Console.WriteLine("Booting up..");
+            Thread.Sleep(2000);
+            Console.WriteLine();
+		
+		}
+        public void powerDown()
+		{
+			Console.WriteLine("Shutting down...");
+            Thread.Sleep(2000);
+		}
+    }
+}
+
