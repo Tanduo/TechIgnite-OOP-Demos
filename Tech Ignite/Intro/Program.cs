260fae87cd8f70e613b7743be11c93d9db704ba5
Ryan Lange <56279086+rpl17@users.noreply.github.com>
2023-10-07 09:26:04 -0500

    Initial commit - Calc console app & intro to variables projs

---
+++ Tech Ignite/Intro/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int num1 = 1;
+        int num2 = 1;
+        int sum = num1 + num2;
+        string add = "Adding " + num1 + "+" + num2;
+        Console.WriteLine(add);
+        Console.WriteLine("= " + sum);
+    }
+}
